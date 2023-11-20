fecd7b2762cab728c277cc38f80dd72fa47d7d4c
Alexis Farmer <alexis.farmer@husqvarnagroup.com>
2023-11-20 08:19:02 +0100

    changed one file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,8 +29,8 @@
       
       const entry = entries.find((entry) => entry.target === canvas)
       if (entry!=undefined) {
-        canvas.width = entry.devicePixelContentBoxSize[0].inlineSize;
-        canvas.height = entry.devicePixelContentBoxSize[0].blockSize;
+        canvas.width = entry.devicePixelContentBoxSize[0].inlineSize
+        canvas.height = entry.devicePixelContentBoxSize[0].blockSize
       }
 
       ctx?.beginPath();
@@ -39,9 +39,33 @@
     });
 
     observer.observe(canvas)
+
   })
+
+  function canvasMouseMove(e: MouseEvent) {
+    const canvas = ( <HTMLCanvasElement>e.target )
+    const ctx = canvas.getContext("2d");
+    const clientWidth = canvas.getBoundingClientRect().right - canvas.getBoundingClientRect().left
+    const clientHeight = canvas.getBoundingClientRect().bottom - canvas.getBoundingClientRect().top
+    const x = e.pageX * canvas.width / clientWidth
+    const y = e.pageY * canvas.height / clientHeight
+    console.log(clientWidth, canvas.width )
+    if (ctx) {
+      const r = 0
+      const g = 0
+      const b = 0
+      const a = 255
+      ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
+      ctx.fillRect( x, y,  2, 2 );
+    }
+    
+    //console.log(e)
+    //let x = e.pageX
+    //let y = e.pageY
+    
+  }
 </script>
 <body>
-    <canvas id="canvas"></canvas>
+    <canvas on:mousemove={canvasMouseMove} on:focus id="canvas"></canvas>
 </body>
 
