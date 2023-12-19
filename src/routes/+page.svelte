b22e6cbfec6b378029fdba1aba22e0d9c86ef7ff
alexis <alexis.farmer@bolder.no>
2023-12-19 14:06:12 +0100

    added timer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
 <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=B612 Mono' rel='stylesheet'>
  
 <style>
 
@@ -9,7 +11,7 @@
     overflow: hidden;
     display: block;
     margin: 0;
-    font-family: 'Montserrat';font-size: 22px;
+    font-family: 'B612 Mono';font-size: 22px;
   }
 
   canvas {
@@ -27,10 +29,12 @@
 
   function drawRoundedRegularPolygon(
     ctx: CanvasRenderingContext2D,
+    radius: number,
     rotation: number,
     cornerPercent: number,
     shadowBlur: number,
     color: string,
+    shadowColor: string, 
     numberOfCorners: number ) {
 
     function getRegularPolygonCorner( index: number, numberOfCorners: number ): number[] {
@@ -45,12 +49,13 @@
 
 
     ctx.save( )
+    ctx.scale( radius, radius )
     ctx.rotate( rotation * Math.PI / 180 )
 
     drawRoundedPolygon( ctx, cornerPercent, corners )
 
     ctx.shadowBlur = shadowBlur
-	  ctx.shadowColor=   color
+	  ctx.shadowColor = shadowColor
 	  ctx.shadowOffsetX = ctx.shadowOffsetY = 0
     ctx.fillStyle = color
     ctx.fill( )
@@ -85,8 +90,8 @@
 
   }
 
-  function drawText( ctx: CanvasRenderingContext2D, text: string ) {
-    ctx.font = "1px Montserrat";
+  function drawText( ctx: CanvasRenderingContext2D, text: string, color: string, scale: number ) {
+    ctx.font = "1px B612 Mono";
     ctx.fillStyle = "white"
     const metrics = ctx.measureText( text )
     const textWidth = metrics.width
@@ -94,32 +99,41 @@
     ctx.shadowBlur = 10
     ctx.shadowColor = "white"
     ctx.shadowOffsetX = ctx.shadowOffsetY = 0
+    ctx.save( )
+    ctx.scale( scale, scale )
+    ctx.fillStyle = color
     ctx.fillText( text, (0-textWidth)/2, (0+actualHeight)/2)
+    ctx.restore( )
   }
 
   onMount(async () => {
-    const canvas = <HTMLCanvasElement> document.getElementById("canvas");
-    const ctx: CanvasRenderingContext2D  = canvas.getContext("2d")!;
-
-    function doDraw() {
-      ctx.canvas.width  = window.innerWidth;
-      ctx.canvas.height = window.innerHeight;
-      ctx.save( )
-      ctx.translate( canvas.width/2, canvas.height/2 )
-      ctx.scale( canvas.height / 2, canvas.height / 2)
-      drawRoundedRegularPolygon( ctx, 45, 33, 20, "#E49641", 3 )
-      drawText( ctx, "11" )
-      ctx.restore( )
-    }
+    const canvas: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("canvas");
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
     function step(time: number) {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      doDraw()
+      console.log( time )
+      draw( ctx, time )
       requestAnimationFrame(step)
     }
 
     requestAnimationFrame(step)
   })
+
+  function draw( ctx: CanvasRenderingContext2D, time: number ) {
+      ctx.canvas.width  = window.innerWidth;
+      ctx.canvas.height = window.innerHeight;
+      const shortestSide = ctx.canvas.width < ctx.canvas.height ? ctx.canvas.width : ctx.canvas.height
+      ctx.save( )
+      ctx.translate( ctx.canvas.width/2, ctx.canvas.height/2 )
+      ctx.scale( shortestSide / 4, shortestSide / 4)
+      drawRoundedRegularPolygon( ctx, 1.3, 45, 33, 20, "#C34D1A", "#00000020", 6 )
+      drawRoundedRegularPolygon( ctx, 1.2, 45, 33, 20, "#DD8B31", "#00000020", 6 )
+      drawRoundedRegularPolygon( ctx, 1.1, 45, 33, 20, "#EEBD6D", "#00000020", 6 )
+      drawRoundedRegularPolygon( ctx, 1.0, 45, 33, 20, "#F2D8AD", "#00000020", 6 )
+      drawText( ctx, ( Math.floor( time / 1000 )%30 ).toString( ), "#EAAB4D", 1 )
+      ctx.restore( )
+    }
+
 </script>
 <body>
     <canvas style="position:absolute;" on:focus id="canvas"></canvas>
